92fec0cfe98fe82a8d3b41a5271b25067e331ea1
Emily-OBrien-NHS <e.obrien6@nhs.net>
2025-11-19 12:57:53 +0000

    Added clinic code and rearranged excel output

--- cancer_wl_model.py
+++ cancer_wl_model.py
@@ -174,134 +174,153 @@ start[['Specialty', 'Clinic Code',
 ################################################################################
                 #####Calculate Each Past Data and Forecast#####
 ################################################################################
+t0 = time.time()
 past_data = []
 output_table = []
-for situation in start.values.tolist():
-    #Update variables
-    WL_start, adds, spec, cc, prior, N_FU = situation
-
-    #If the row is for a specialty and/or appointment type filtering, add these
-    #to a list of conditions for past and slots datasets.
-    hist_conds = []
-    slots_conds = []
-    if spec != 'All':
-        hist_conds.append(cancer_wl['Specialty'] == spec)
-        slots_conds.append(cancer_slots['Specialty'] == spec)
+for cc in start['Clinic Code'].drop_duplicates().values.tolist():
+    #If data is not all, filter to that clinic code
     if cc != 'All':
-        hist_conds.append(cancer_wl['Clinic Code'] == cc)
-        slots_conds.append(cancer_slots['Clinic Code'] == cc)
-    if prior != 'All':
-        hist_conds.append(cancer_wl['Priority'] == prior)
-    if N_FU != 'All':
-        hist_conds.append(cancer_wl['New/Follow Up'] == N_FU)
-        slots_conds.append(cancer_slots['New/Follow Up'].isin(['Undefined', N_FU]))
-    
-    
-    ######################################################Hist Data
-    #Filter the historical dataset based on the conditions
-    filter_hist = cancer_wl.copy()
-    for cond in hist_conds:
-        filter_hist = filter_hist.loc[cond].copy()
-    
-    #If data is not at a granular level, we need to aggregate it up to get the
-    #data for this grouping
-    if len(filter_hist) > 6:
-        agg_past = (filter_hist.groupby('Week End', as_index=False)
-                    [['Waitlist Size', 'Waitlist Additions', 'Attendances']]
-                    .sum().values.tolist())
+        cc_filter_hist = cancer_wl.loc[cancer_wl['Clinic Code'] == cc].copy()
+        cc_filter_slots = cancer_slots.loc[cancer_slots['Clinic Code'] == cc].copy()
     else:
-        agg_past = filter_hist[['Week End', 'Waitlist Size',
-                   'Waitlist Additions', 'Attendances']].values.tolist()
+        cc_filter_hist = cancer_wl.copy()
+        cc_filter_slots = cancer_slots.copy()
+    #Filter the start table to those combinations
+    start_filter = start.loc[start['Clinic Code'] == cc].drop('Clinic Code', axis=1)
 
-    #For each of the past 6 weeks, append the data to the past data list
-    for row in agg_past:
-        week, wl_size, add, att = row
-        past_data.append([week, spec, cc, prior, N_FU, wl_size, add, att, 'Past', 'Y'])
+    #Evaluate each situation for that clinic code
+    for situation in start_filter.values.tolist():
+        #Update variables
+        WL_start, adds, spec, prior, N_FU = situation
+        #print(situation)
 
-    ######################################################Forecast
-    #Filter the slots dataset based on the conditions
-    filter_slots = cancer_slots.copy()
-    for cond in slots_conds:
-        filter_slots = filter_slots.loc[cond].copy()
-
-    #If multiple rows for the Weeks, then group up by week
-    if len(filter_slots) > 3:
-        all_filter_slots = filter_slots.groupby('Week End')['Slots'].sum()
-
-        #Do a version with undefined removed if it exists to produce 2 forecasts.
-        if 'Undefined' in filter_slots['New/Follow Up'].values:
-            filter_slots = (filter_slots.loc[
-                            filter_slots['New/Follow Up']!= 'Undefined']
-                            .groupby('Week End')['Slots'].sum())
-        #If no undefined, then both forecasts will be the same.
+        #If the row is for a specialty and/or appointment type filtering, add these
+        #to a list of conditions for past and slots datasets.
+        hist_conds = []
+        slots_conds = []
+        if spec != 'All':
+            hist_conds.append(cancer_wl['Specialty'] == spec)
+            slots_conds.append(cancer_slots['Specialty'] == spec)
+    # if cc != 'All':
+        #    hist_conds.append(cancer_wl['Clinic Code'] == cc)
+        #   slots_conds.append(cancer_slots['Clinic Code'] == cc)
+        if prior != 'All':
+            hist_conds.append(cancer_wl['Priority'] == prior)
+        if N_FU != 'All':
+            hist_conds.append(cancer_wl['New/Follow Up'] == N_FU)
+            slots_conds.append(cancer_slots['New/Follow Up'].isin(['Undefined', N_FU]))
+        
+        
+        ######################################################Hist Data
+        #Filter the historical dataset based on the conditions
+        #filter_hist = cancer_wl.copy()
+        filter_hist = cc_filter_hist.copy()
+        for cond in hist_conds:
+            filter_hist = filter_hist.loc[cond].copy()
+        
+        #If data is not at a granular level, we need to aggregate it up to get the
+        #data for this grouping
+        if len(filter_hist) > 6:
+            agg_past = (filter_hist.groupby('Week End', as_index=False)
+                        [['Waitlist Size', 'Waitlist Additions', 'Attendances']]
+                        .sum().values.tolist())
         else:
-            filter_slots = all_filter_slots
-    else:
-        #If 3 or less rows, then all filter slots is the same as filter slots.
-        all_filter_slots = filter_slots
-    
-    #Calculate future wait list position
-    fut_WL_inc_undef = []
-    fut_WL_exc_undef = []
-    WL_inc_undef = WL_start
-    WL_exc_undef = WL_start
-    for week in fut_weeks:
-        #Calculate the future waitlist including undefined
-        try: #If week isnt in the data, make 0
-            slots_inc_undef = all_filter_slots.loc[week]
-        except:
-            slots_inc_undef = 0
-        new_WL_inc_undef = WL_inc_undef + adds - slots_inc_undef
-        fut_WL_inc_undef.append(new_WL_inc_undef)
-        WL_inc_undef = new_WL_inc_undef
+            agg_past = filter_hist[['Week End', 'Waitlist Size',
+                    'Waitlist Additions', 'Attendances']].values.tolist()
 
-        output_table.append(
-                     [week, spec, cc, prior, N_FU, round(new_WL_inc_undef),
-                      round(adds), slots_inc_undef, 'Forecast', 'Y'])
+        #For each of the past 6 weeks, append the data to the past data list
+        for row in agg_past:
+            week, wl_size, add, att = row
+            past_data.append([week, spec, cc, prior, N_FU, wl_size, add, att, 'Past', np.nan])
 
-        #Calculate the future waitlist excluding undefined
-        try: #If week isnt in the data, make 0
-            slots_exc_undef = filter_slots.loc[week]
-        except:
-            slots_exc_undef = 0
-        new_WL_exc_undef = WL_exc_undef + adds - slots_exc_undef
-        fut_WL_exc_undef.append(new_WL_exc_undef)
-        WL_exc_undef = new_WL_exc_undef
-        output_table.append(
-                     [week, spec, cc, prior, N_FU, round(new_WL_exc_undef),
-                      round(adds), slots_exc_undef, 'Forecast', 'N'])
+        ######################################################Forecast
+        #Filter the slots dataset based on the conditions
+        #filter_slots = cancer_slots.copy()
+        filter_slots = cc_filter_slots.copy()
+        for cond in slots_conds:
+            filter_slots = filter_slots.loc[cond].copy()
+
+        #If multiple rows for the Weeks, then group up by week
+        if len(filter_slots) > 3:
+            all_filter_slots = filter_slots.groupby('Week End')['Slots'].sum()
+
+            #Do a version with undefined removed if it exists to produce 2 forecasts.
+            if 'Undefined' in filter_slots['New/Follow Up'].values:
+                filter_slots = (filter_slots.loc[
+                                filter_slots['New/Follow Up']!= 'Undefined']
+                                .groupby('Week End')['Slots'].sum())
+            #If no undefined, then both forecasts will be the same.
+            else:
+                filter_slots = all_filter_slots
+        else:
+            #If 3 or less rows, then all filter slots is the same as filter slots.
+            all_filter_slots = filter_slots
+        
+        #Calculate future wait list position
+        fut_WL_inc_undef = []
+        fut_WL_exc_undef = []
+        WL_inc_undef = WL_start
+        WL_exc_undef = WL_start
+        for week in fut_weeks:
+            #Calculate the future waitlist including undefined
+            try: #If week isnt in the data, make 0
+                slots_inc_undef = all_filter_slots.loc[week]
+            except:
+                slots_inc_undef = 0
+            new_WL_inc_undef = WL_inc_undef + adds - slots_inc_undef
+            fut_WL_inc_undef.append(new_WL_inc_undef)
+            WL_inc_undef = new_WL_inc_undef
+
+            output_table.append(
+                        [week, spec, cc, prior, N_FU, round(new_WL_inc_undef),
+                        round(adds), slots_inc_undef, 'Forecast', 'Y'])
+
+            #Calculate the future waitlist excluding undefined
+            try: #If week isnt in the data, make 0
+                slots_exc_undef = filter_slots.loc[week]
+            except:
+                slots_exc_undef = 0
+            new_WL_exc_undef = WL_exc_undef + adds - slots_exc_undef
+            fut_WL_exc_undef.append(new_WL_exc_undef)
+            WL_exc_undef = new_WL_exc_undef
+            output_table.append(
+                        [week, spec, cc, prior, N_FU, round(new_WL_exc_undef),
+                        round(adds), slots_exc_undef, 'Forecast', 'N'])
         
 columns = ['Week End', 'Specialty', 'Clinic Code', 'Priority', 'New/Follow Up',
            'Waitlist Size', 'Waitlist Additions', 'Attendances', 'Past/Future',
            'Including Undefined']
 #Put together historical summarised data, create a copy with undefined column
 historical = pd.DataFrame(past_data, columns = columns)
-historical_exc_undef = historical.copy()
-historical_exc_undef['Including Undefined'] = 'N'
+
+#historical_exc_undef = historical.copy()
+#historical_exc_undef['Including Undefined'] = 'N'
 #Create table of forecasts
 forecast = pd.DataFrame(output_table, columns = columns)
 #concat all 3 tables into one output
-wl_full_dataset = pd.concat([historical, historical_exc_undef, forecast])
+#wl_full_dataset = pd.concat([historical, historical_exc_undef, forecast])
+wl_full_dataset = pd.concat([historical, forecast])
 wl_full_dataset['Week End'] = pd.to_datetime(wl_full_dataset['Week End']).astype(str)
 wl_full_dataset['Lookup Col'] = (wl_full_dataset['Week End']
                                  + wl_full_dataset['Specialty']
+                                 + wl_full_dataset['Clinic Code']
                                  + wl_full_dataset['Priority']
                                  + wl_full_dataset['New/Follow Up']
-                                 + wl_full_dataset['Including Undefined'])
+                                 + wl_full_dataset['Including Undefined'].fillna(''))
 wl_full_dataset =  wl_full_dataset[
-                    ['Week End', 'Specialty', 'Priority', 'New/Follow Up',
+                    ['Week End', 'Specialty', 'Clinic Code', 'Priority', 'New/Follow Up',
                      'Including Undefined', 'Lookup Col', 'Waitlist Size',
                      'Waitlist Additions', 'Attendances', 'Past/Future']].copy()
 
 #Create a table template with the dates and nans to be filled in by excel formulae
-wl_tabletemplate = wl_full_dataset.drop_duplicates(subset='Week End').sort_values(by='Week End')
-cols = [i for i in wl_tabletemplate.columns if i != 'Week End']
+wl_tabletemplate = wl_full_dataset[['Week End', 'Past/Future', 'Waitlist Size',
+                                    'Waitlist Additions', 'Attendances']
+                   ].drop_duplicates(subset='Week End').sort_values(by='Week End')
+cols = [i for i in wl_tabletemplate.columns if (i != 'Week End' and i != 'Past/Future')]
 wl_tabletemplate[cols] = np.nan
 
 t1=time.time()
 print(f'Done in {t1-t0}')
-
-
 
 
 ################################################################################
@@ -330,66 +349,83 @@ filter_format2 = workbook.add_format({'font_size':14, 'bold':True, 'align':'cent
 
 ######Dashboard
 wl_tabletemplate.to_excel(writer, sheet_name='Dash', index=False,
-                          startrow=1, startcol=4)
+                          startrow=10, startcol=1)
 
 #White background and default column widths
 dash_ws.set_column('A:AE', 15, bg_format)
 dash_ws.set_column('A:A', 4, bg_format)
-dash_ws.set_column('D:D', 4, bg_format)
+dash_ws.set_column('G:G', 4, bg_format)
 dash_ws.set_row(2, None,  bg_format)
 for row in range(0, 9):
     dash_ws.set_row(row + 2, 21)
 
     ##Filter section
 #definition column
-dash_ws.set_column('B:C', 30, bg_format)
-dash_ws.merge_range('B2:C2', 'Filters', header_format)
-dash_ws.write('B3', 'Specialty', filter_format1)
-dash_ws.write('B4', 'Priority', filter_format1)
-dash_ws.write('B5', 'New/Follow Up', filter_format1)
-dash_ws.write('B6', 'Including Undefined', filter_format1)
+#dash_ws.set_column('B:C', 30, bg_format)
+dash_ws.merge_range('B2:E2', 'Filters', header_format)
+dash_ws.merge_range('B3:C3', 'Specialty', filter_format1)
+dash_ws.merge_range('B4:C4', 'Clinic Code', filter_format1)
+dash_ws.merge_range('B5:C5', 'Priority', filter_format1)
+dash_ws.merge_range('B6:C6', 'New/Follow Up', filter_format1)
+dash_ws.merge_range('B7:C7', 'Including Undefined', filter_format1)
+dash_ws.merge_range('B8:E8', '=D3&D4&D5&D6', filter_format1)
 #selection column
 no_spec = wl_full_dataset['Specialty'].nunique() + 1
-dash_ws.data_validation('C3', {'validate':'list', 'source':f"'Look Up'!A2:A{no_spec}"})
-dash_ws.write('C3', 'All', filter_format2)
-dash_ws.data_validation('C4', {'validate':'list', 'source':wl_full_dataset['Priority'].drop_duplicates().tolist()})
-dash_ws.write('C4', 'All', filter_format2)
-dash_ws.data_validation('C5', {'validate':'list', 'source':wl_full_dataset['New/Follow Up'].drop_duplicates().tolist()})
-dash_ws.write('C5', 'All', filter_format2)
-dash_ws.data_validation('C6', {'validate':'list', 'source':wl_full_dataset['Including Undefined'].drop_duplicates().tolist()})
-dash_ws.write('C6', 'Y', filter_format2)
+no_cc = wl_full_dataset['Clinic Code'].nunique() + 1
+dash_ws.data_validation('D3', {'validate':'list', 'source':f"'Look Up'!A2:A{no_spec}"})
+dash_ws.merge_range('D3:E3', 'All', filter_format2)
+dash_ws.data_validation('D4', {'validate':'list', 'source':f"'Look Up'!C2:C{no_cc}"})
+dash_ws.merge_range('D4:E4', 'All', filter_format2)
+dash_ws.data_validation('D5', {'validate':'list', 'source':wl_full_dataset['Priority'].drop_duplicates().tolist()})
+dash_ws.merge_range('D5:E5', 'All', filter_format2)
+dash_ws.data_validation('D6', {'validate':'list', 'source':wl_full_dataset['New/Follow Up'].drop_duplicates().tolist()})
+dash_ws.merge_range('D6:E6', 'All', filter_format2)
+dash_ws.data_validation('D7', {'validate':'list', 'source':wl_full_dataset['Including Undefined'].drop_duplicates().tolist()})
+dash_ws.merge_range('D7:E7', 'Y', filter_format2)
 
     ##Table section
 #Populate the table with vlookups
-for row in range(3, 12):
-    dash_ws.write(f'F{row}', '=C3')
-    dash_ws.write(f'G{row}', '=C4')
-    dash_ws.write(f'H{row}', '=C5')
-    dash_ws.write(f'I{row}', '=C6')
-    dash_ws.write(f'J{row}', f'=E{row} & C3 & C4 & C5 & C6')
-    dash_ws.write(f'K{row}', f"=VLOOKUP(J{row},'Full Data'!F:J,2,0)")
-    dash_ws.write(f'L{row}', f"=VLOOKUP(J{row},'Full Data'!F:J,3,0)")
-    dash_ws.write(f'M{row}', f"=VLOOKUP(J{row},'Full Data'!F:J,4,0)")
-    dash_ws.write(f'N{row}', f"=VLOOKUP(J{row},'Full Data'!F:J,5,0)")
+for row in range(12, 18):
+    dash_ws.write(f'D{row}', f"=VLOOKUP(B{row}&B8,'Full Data'!G:J,2,0)")
+    dash_ws.write(f'E{row}', f"=VLOOKUP(B{row}&B8,'Full Data'!G:J,3,0)")
+    dash_ws.write(f'F{row}', f"=VLOOKUP(B{row}&B8,'Full Data'!G:J,4,0)")
+for row in range(18, 21):
+    dash_ws.write(f'D{row}', f"=VLOOKUP(B{row}&B8&D7,'Full Data'!G:J,2,0)")
+    dash_ws.write(f'E{row}', f"=VLOOKUP(B{row}&B8&D7,'Full Data'!G:J,3,0)")
+    dash_ws.write(f'F{row}', f"=VLOOKUP(B{row}&B8&D7,'Full Data'!G:J,4,0)")
 
     ##Line Graph section
 WL_chart = workbook.add_chart({'type':'line'})
-WL_chart.add_series({'name':'Wait List', 'categories':'=Dash!$E$3:$E$11', 'values':'=Dash!$K$3:$K$11', 'data_labels': {'value': True, 'position': 'above'}, 'smooth':True, 'marker': {'type': 'automatic'},})
+WL_chart.add_series({'name':'Wait List',
+                     'categories':'=Dash!$B$12:$B$20',
+                     'values':'=Dash!$D$12:$D$20',
+                     'data_labels': {'value': True,
+                                     'position': 'above'},
+                     'smooth':True,
+                     'marker': {'type': 'automatic'},})
 WL_chart.set_title({'Name':'Wait List'})
-dash_ws.insert_chart('B13', WL_chart, {'x_scale': 3.75, 'y_scale': 0.75})
-
+dash_ws.insert_chart('H2', WL_chart, {'x_scale': 2.7, 'y_scale': 1.1})
 
     #Bar Chart section
 att_add_chart = workbook.add_chart({'type':'column'})
-att_add_chart.add_series({'name':'Additions', 'categories':'=Dash!$E$3:$E$11', 'values':'=Dash!$L$3:$L$11', 'data_labels': {'value': True}})
-att_add_chart.add_series({'name':'Attendances', 'categories':'=Dash!$E$3:$E$11', 'values':'=Dash!$M$3:$M$11', 'data_labels': {'value': True}})
+att_add_chart.add_series({'name':'Additions',
+                          'categories':'=Dash!$B$12:$B$20',
+                          'values':'=Dash!$E$12:$E$20',
+                          'data_labels': {'value': True}})
+att_add_chart.add_series({'name':'Attendances',
+                          'categories':'=Dash!$B$12:$B$20',
+                          'values':'=Dash!$F$12:$F$20',
+                          'data_labels': {'value': True}})
 att_add_chart.set_title({'Name':'Additions and Attendances'})
-dash_ws.insert_chart('B24', att_add_chart, {'x_scale': 3.75, 'y_scale': 0.75})
+dash_ws.insert_chart('H15', att_add_chart, {'x_scale': 2.7, 'y_scale': 1.1})
 
 ######Full Data Set
 wl_full_dataset.to_excel(writer, sheet_name='Full Data', index=False)
 
 ######Lookup sheet
 wl_full_dataset['Specialty'].drop_duplicates().to_excel(writer, sheet_name='Look Up', index=False)
+wl_full_dataset['Clinic Code'].drop_duplicates().to_excel(writer, sheet_name='Look Up', index=False, startcol=2)
 
 writer.close()
+
+X=5
